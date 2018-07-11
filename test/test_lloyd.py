c5d32256add158228767101a1de5a09d078106fb
Nico Schlömer <nico.schloemer@gmail.com>
2018-07-11 23:44:32 +0200

    lloyd: test parametrization

--- test/test_lloyd.py
+++ test/test_lloyd.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 #
 import numpy
+import pytest
 
 import optimesh
 
 from meshes import simple1, pacman
 
 
-def test_simple_lloyd(max_num_steps=5):
-    X, cells = simple1()
+@pytest.mark.parametrize("mesh, ref1, ref2, refi", [
+    (simple1, 4.985355657854027, 2.1179164560036154, 1.0),
+    (pacman, 1939.1198108068188, 75.94965207932323, 5.0),
+])
+def test_simple_lloyd(mesh, ref1, ref2, refi):
+    X, cells = mesh()
 
     X, cells = optimesh.lloyd(X, cells, 1.0e-2, 100, fcc_type="boundary", verbosity=2)
 
@@ -19,38 +24,7 @@ def test_simple_lloyd(max_num_steps=5):
     normi = numpy.linalg.norm(nc, ord=numpy.inf)
 
     tol = 1.0e-12
-    ref = 4.985355657854027
-    assert abs(norm1 - ref) < tol * ref
-    ref = 2.1179164560036154
-    assert abs(norm2 - ref) < tol * ref
-    ref = 1.0
-    assert abs(normi - ref) < tol * ref
-
+    assert abs(norm1 - ref1) < tol * ref1
+    assert abs(norm2 - ref2) < tol * ref2
+    assert abs(normi - refi) < tol * refi
     return
-
-
-def test_pacman_lloyd(max_num_steps=1000):
-    X, cells = pacman()
-
-    X, _ = optimesh.lloyd(X, cells, 1.0e-2, 100, fcc_type="boundary", verbosity=1)
-
-    # Test if we're dealing with the mesh we expect.
-    nc = X.flatten()
-    norm1 = numpy.linalg.norm(nc, ord=1)
-    norm2 = numpy.linalg.norm(nc, ord=2)
-    normi = numpy.linalg.norm(nc, ord=numpy.inf)
-
-    tol = 1.0e-12
-    ref = 1939.1198108068188
-    assert abs(norm1 - ref) < tol * ref
-    ref = 75.94965207932323
-    assert abs(norm2 - ref) < tol * ref
-    ref = 5.0
-    assert abs(normi - ref) < tol * ref
-
-    return
-
-
-if __name__ == "__main__":
-    # test_simple_lloyd(
-    test_pacman_lloyd(max_num_steps=100)
