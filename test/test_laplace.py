9aa3fffdac4c66b4eb3688960c09ffddd922cc5c
Nico Schlömer <nico.schloemer@gmail.com>
2018-07-11 23:41:56 +0200

    laplace tests parametrization

--- test/test_laplace.py
+++ test/test_laplace.py
@@ -1,38 +1,21 @@
 # -*- coding: utf-8 -*-
 #
 import numpy
+import pytest
 
 import optimesh
 
 from meshes import simple1, pacman
 
 
-def test_simple_fixed_point(num_steps=10):
-    X, cells = simple1()
+@pytest.mark.parametrize("mesh, ref1, ref2, refi", [
+    (simple1, 5.0, 2.1213203435596424, 1.0),
+    (pacman, 1917.9950540725958, 74.99386491032608, 5.0),
+])
+def test_fixed_point(mesh, ref1, ref2, refi):
+    X, cells = mesh()
 
-    X, cells = optimesh.laplace.fixed_point(X, cells, 0.0, num_steps)
-
-    # Test if we're dealing with the mesh we expect.
-    nc = X.flatten()
-    norm1 = numpy.linalg.norm(nc, ord=1)
-    norm2 = numpy.linalg.norm(nc, ord=2)
-    normi = numpy.linalg.norm(nc, ord=numpy.inf)
-
-    tol = 1.0e-12
-    ref = 5.0
-    assert abs(norm1 - ref) < tol * ref
-    ref = 2.1213203435596424
-    assert abs(norm2 - ref) < tol * ref
-    ref = 1.0
-    assert abs(normi - ref) < tol * ref
-
-    return
-
-
-def test_simple_linear_solve(num_steps=1):
-    X, cells = simple1()
-
-    X, cells = optimesh.laplace.linear_solve(X, cells, 0.0, num_steps)
+    X, cells = optimesh.laplace.fixed_point(X, cells, 0.0, 10)
 
     # Test if we're dealing with the mesh we expect.
     nc = X.flatten()
@@ -41,20 +24,20 @@ def test_simple_linear_solve(num_steps=1):
     normi = numpy.linalg.norm(nc, ord=numpy.inf)
 
     tol = 1.0e-12
-    ref = 5.0
-    assert abs(norm1 - ref) < tol * ref
-    ref = 2.1213203435596424
-    assert abs(norm2 - ref) < tol * ref
-    ref = 1.0
-    assert abs(normi - ref) < tol * ref
-
+    assert abs(norm1 - ref1) < tol * ref1
+    assert abs(norm2 - ref2) < tol * ref2
+    assert abs(normi - refi) < tol * refi
     return
 
 
-def test_pacman_fixed_point(num_steps=10):
-    X, cells = pacman()
+@pytest.mark.parametrize("mesh, ref1, ref2, refi", [
+    (simple1, 5.0, 2.1213203435596424, 1.0),
+    (pacman, 1861.1845672600928, 73.12639677872326, 5.0),
+])
+def test_linear_solve(mesh, ref1, ref2, refi):
+    X, cells = mesh()
 
-    X, _ = optimesh.laplace.fixed_point(X, cells, 0.0, num_steps, verbosity=1)
+    X, cells = optimesh.laplace.linear_solve(X, cells, 0.0, 10)
 
     # Test if we're dealing with the mesh we expect.
     nc = X.flatten()
@@ -63,37 +46,7 @@ def test_pacman_fixed_point(num_steps=10):
     normi = numpy.linalg.norm(nc, ord=numpy.inf)
 
     tol = 1.0e-12
-    ref = 1917.9950540725958
-    assert abs(norm1 - ref) < tol * ref
-    ref = 74.99386491032608
-    assert abs(norm2 - ref) < tol * ref
-    ref = 5.0
-    assert abs(normi - ref) < tol * ref
-
+    assert abs(norm1 - ref1) < tol * ref1
+    assert abs(norm2 - ref2) < tol * ref2
+    assert abs(normi - refi) < tol * refi
     return
-
-
-def test_pacman_linear_solve(num_steps=10):
-    X, cells = pacman()
-
-    X, _ = optimesh.laplace.fixed_point(X, cells, 0.0, num_steps, verbosity=1)
-
-    # Test if we're dealing with the mesh we expect.
-    nc = X.flatten()
-    norm1 = numpy.linalg.norm(nc, ord=1)
-    norm2 = numpy.linalg.norm(nc, ord=2)
-    normi = numpy.linalg.norm(nc, ord=numpy.inf)
-
-    tol = 1.0e-12
-    ref = 1917.9950540725958
-    assert abs(norm1 - ref) < tol * ref
-    ref = 74.99386491032608
-    assert abs(norm2 - ref) < tol * ref
-    ref = 5.0
-    assert abs(normi - ref) < tol * ref
-
-    return
-
-
-if __name__ == "__main__":
-    test_pacman_linear_solve(num_steps=5)
