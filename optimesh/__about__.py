4df3ae8a2ba4959ec3a627022a70d769b117ddac
Nico Schlömer <nico.schloemer@gmail.com>
2019-03-27 11:05:46 +0100

    version, status bump

--- optimesh/__about__.py
+++ optimesh/__about__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 #
 
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 __author__ = "Nico Schlömer"
 __author_email__ = "nico.schloemer@gmail.com"
 __website__ = "https://github.com/nschloe/optimesh"
-__status__ = "Development Status :: 3 - Alpha"
+__status__ = "Development Status :: 4 - Beta"
 __license__ = "License :: OSI Approved :: MIT License"
