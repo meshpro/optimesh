42e24e13831962699f8bb0122e639119390e828a
Nico Schlömer <nico.schloemer@gmail.com>
2019-07-28 11:17:17 +0200

    update full hessian cvt

--- optimesh/cvt/full_hessian.py
+++ optimesh/cvt/full_hessian.py
@@ -3,56 +3,55 @@
 import numpy
 import scipy.sparse
 
-from .ghosted_mesh import GhostedMesh
-from .helpers import jac_uniform
+from meshplex import MeshTri
 
 from ..helpers import runner
+from .helpers import jac_uniform
 
 
-def quasi_newton_uniform_full(points, cells, *args, omega=1.0, **kwargs):
+def quasi_newton_uniform_full(points, cells, *args, **kwargs):
     def get_new_points(mesh):
         # TODO need copy?
         x = mesh.node_coords.copy()
-        x += update(ghosted_mesh, omega)
-        # update ghosts
-        x[ghosted_mesh.is_ghost_point] = ghosted_mesh.reflect_ghost(
-            x[ghosted_mesh.mirrors]
-        )
+        x += update(mesh)
         return x
 
-    ghosted_mesh = GhostedMesh(points, cells)
+    mesh = MeshTri(points, cells)
 
-    runner(
+    out = runner(
         get_new_points,
-        ghosted_mesh,
+        mesh,
         *args,
         **kwargs,
-        get_stats_mesh=lambda mesh: ghosted_mesh.get_unghosted_mesh(),
         method_name="Centroidal Voronoi Tesselation (CVT), uniform density, exact-Hessian variant"
     )
-
-    mesh = ghosted_mesh.get_unghosted_mesh()
-    # mesh = ghosted_mesh
+    print(out)
     return mesh.node_coords, mesh.cells["nodes"]
 
 
-def update(ghosted_mesh, omega):
-    mesh = ghosted_mesh
+def update(mesh):
+    # Exclude all cells which have a too negative covolume-edgelength ratio. This is
+    # necessary to prevent nodes to be dragged outside of the domain by very flat
+    # cells on the boundary.
+    # There are other possible heuristics too. For example, one could restrict the
+    # mask to cells at or near the boundary.
+    mask = numpy.any(mesh.ce_ratios < -0.5, axis=0)
 
-    ei_outer_ei = numpy.einsum(
-        "ijk, ijl->ijkl", mesh.half_edge_coords, mesh.half_edge_coords
-    )
+    hec = mesh.half_edge_coords[:, ~mask]
+    ei_outer_ei = numpy.einsum("ijk, ijl->ijkl", hec, hec)
 
     # create approximate Hessian
     row_idx = []
     col_idx = []
     vals = []
 
-    M = -0.5 * ei_outer_ei * mesh.ce_ratios[..., None, None]
+    M = -0.5 * ei_outer_ei * mesh.ce_ratios[:, ~mask, None, None]
+
     # Scale the off-diagonal blocks with some factor. If omega == 1, this is the
     # Hessian. Unfortunately, it seems that the Newton domain of convergence is small.
     # Relaxation makes the method more robust.
-    M2 = M * omega
+    # M2 = M * omega
+    # M2 = M
 
     block_size = M.shape[2]
     assert block_size == M.shape[3]
@@ -62,34 +61,35 @@ def update(ghosted_mesh, omega):
         # (i0, i0) block
         for i in range(block_size):
             for j in range(block_size):
-                row_idx += [block_size * mesh.idx_hierarchy[0, k] + i]
-                col_idx += [block_size * mesh.idx_hierarchy[0, k] + j]
+                row_idx += [block_size * mesh.idx_hierarchy[0, k, ~mask] + i]
+                col_idx += [block_size * mesh.idx_hierarchy[0, k, ~mask] + j]
                 vals += [M[k, :, i, j]]
         # (i1, i1) block
         for i in range(block_size):
             for j in range(block_size):
-                row_idx += [block_size * mesh.idx_hierarchy[1, k] + i]
-                col_idx += [block_size * mesh.idx_hierarchy[1, k] + j]
+                row_idx += [block_size * mesh.idx_hierarchy[1, k, ~mask] + i]
+                col_idx += [block_size * mesh.idx_hierarchy[1, k, ~mask] + j]
                 vals += [M[k, :, i, j]]
         # (i0, i1) block
         for i in range(block_size):
             for j in range(block_size):
-                row_idx += [block_size * mesh.idx_hierarchy[0, k] + i]
-                col_idx += [block_size * mesh.idx_hierarchy[1, k] + j]
-                vals += [M2[k, :, i, j]]
+                row_idx += [block_size * mesh.idx_hierarchy[0, k, ~mask] + i]
+                col_idx += [block_size * mesh.idx_hierarchy[1, k, ~mask] + j]
+                vals += [M[k, :, i, j]]
         # (i1, i0) block
         for i in range(block_size):
             for j in range(block_size):
-                row_idx += [block_size * mesh.idx_hierarchy[1, k] + i]
-                col_idx += [block_size * mesh.idx_hierarchy[0, k] + j]
-                vals += [M2[k, :, i, j]]
+                row_idx += [block_size * mesh.idx_hierarchy[1, k, ~mask] + i]
+                col_idx += [block_size * mesh.idx_hierarchy[0, k, ~mask] + j]
+                vals += [M[k, :, i, j]]
 
     # add diagonal
-    n = mesh.control_volumes.shape[0]
+    cv = mesh.get_control_volumes(cell_mask=mask)
+    n = cv.shape[0]
     for k in range(block_size):
         row_idx.append(block_size * numpy.arange(n) + k)
         col_idx.append(block_size * numpy.arange(n) + k)
-        vals.append(2 * mesh.control_volumes)
+        vals.append(2 * cv)
 
     row_idx = numpy.concatenate(row_idx)
     col_idx = numpy.concatenate(col_idx)
@@ -101,34 +101,26 @@ def update(ghosted_mesh, omega):
 
     # Transform to CSR format for efficiency
     matrix = matrix.tocsr()
+    #
+    rhs = -jac_uniform(mesh, mask)
 
     # Apply Dirichlet conditions.
-    # Set all Dirichlet rows to 0.
-    i_boundary = numpy.where(mesh.is_boundary_node)[0]
-    for i in i_boundary:
+    # Set all Dirichlet rows to 0. When using a cell mask, it can happen that some nodes
+    # don't get any contribution at all because they are adjacent only to masked cells.
+    # Reset those, too.
+    idx = numpy.any(numpy.isnan(rhs), axis=1) | mesh.is_boundary_node
+    i_reset = numpy.where(idx)[0]
+    for i in i_reset:
         for k in range(block_size):
             s = block_size * i + k
             matrix.data[matrix.indptr[s] : matrix.indptr[s + 1]] = 0.0
     # Set the diagonal and RHS.
     d = matrix.diagonal()
     for k in range(block_size):
-        d[block_size * i_boundary + k] = 1.0
+        d[block_size * i_reset + k] = 1.0
     matrix.setdiag(d)
-    #
-    rhs = -jac_uniform(mesh)
-    rhs[i_boundary] = 0.0
+    rhs[i_reset] = 0.0
     rhs = rhs.reshape(-1)
-
-    # TODO Apply ghost conditions.
-    # The ghost equation is
-    #
-    #  pg = p0 + 2 * ((p1 - p0) - <p1 - p0, p2 - p1> / <p2 - p1, p2 - p2> * (p2 - p1)
-    #
-    # where p0 is the point that is reflected, and p1, p2 form the reflexion axis. The
-    # Jacobian of the expression is
-    #
-    # d pg / d p0 = -I + 2 * (p2 - p1) (p2 - p1)^T / (p2 - p1)^T (p2 - p1)
-    #
 
     out = scipy.sparse.linalg.spsolve(matrix, rhs)
     # import pyamg
