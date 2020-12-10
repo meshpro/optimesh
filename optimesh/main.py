afb45a195d533c8d384b024b2726e613477f856a
Nico Schlömer <nico.schloemer@gmail.com>
2020-12-10 21:37:57 +0100

    remove duplicate code

---
+++ optimesh/main.py
@@ -0,0 +1,190 @@
+import meshplex
+import numpy
+
+from . import cpt, cvt, laplace
+from .helpers import print_stats
+
+methods = {
+    "lloyd": cvt.lloyd,
+    "cvt (diagonal)": cvt.lloyd,
+    "cvt (block-diagonal)": cvt.block_diagonal,
+    "cvt (full)": cvt.full,
+    #
+    "cpt (linear solve)": cpt.linear_solve,
+    "cpt (fixed-point)": cpt.fixed_point,
+    "cpt (quasi-newton)": cpt.quasi_newton,
+    #
+    "laplace": laplace,
+}
+
+# mesh = meshplex.MeshTri(points, cells)
+# pts = optimesh.cpt.get_new_points(mesh, "linear solve")
+# pts = optimesh.cpt.get_new_points(mesh, "fixed-point")
+# pts = optimesh.cpt.get_new_points(mesh, "quasi-newton")
+# pts = optimesh.cpt.get_new_points(mesh)
+#
+# optimesh.cpt.optimize(mesh, "quasi-newton", 1.0e-10, 50)
+# optimesh.cpt.optimize(mesh, "fixed-point")
+#
+# optimesh.laplace.get_new_points(mesh)
+# optimesh.laplace.optimize(mesh)
+#
+# optimesh.lloyd.get_new_points(mesh)
+# optimesh.lloyd.optimize(mesh)
+#
+#
+# optimesh.get_new_points(mesh, "lloyd")
+#
+# optimesh.optimize(mesh, "lloyd", omega=2.0)
+# optimesh.optimize(mesh, "laplace")  # dp
+# optimesh.optimize(mesh, "laplace (linear solve)")
+#
+# optimesh.optimize(mesh, "cvt (diagonal)")
+# optimesh.optimize(mesh, "cvt (block-diagonal)")
+# optimesh.optimize(mesh, "cvt (full)")
+#
+# optimesh.optimize(mesh, "cpt (linear solve)")  # dp
+# optimesh.optimize(mesh, "cpt (fixed-point)")
+# optimesh.optimize(mesh, "cpt (quasi-newton)")
+#
+# # optimesh.optimize(mesh, "odt (fixed-point density-preserving)")
+# optimesh.optimize(mesh, "odt (fixed-point)")
+# optimesh.optimize(mesh, "odt (bfgs)")
+
+
+def get_new_points(mesh, method: str):
+    return methods[method.lower()].get_new_points(mesh)
+
+
+def optimize(mesh, method: str, *args, **kwargs):
+    return _optimize(methods[method.lower()].get_new_points, mesh, *args, **kwargs)
+
+
+def optimize_points_cells(X, cells, method: str, *args, **kwargs):
+    mesh = meshplex.MeshTri(X, cells)
+    optimize(mesh, method, *args, **kwargs)
+    return mesh.points, mesh.cells["points"]
+
+
+def _optimize(
+    get_new_points,
+    mesh,
+    tol: float,
+    max_num_steps,
+    omega: float = 1.0,
+    method_name=None,
+    verbose=False,
+    callback=None,
+    step_filename_format=None,
+    implicit_surface=None,
+    implicit_surface_tol=1.0e-10,
+    boundary_step=None,
+):
+    k = 0
+
+    if verbose:
+        print("\nBefore:")
+        print_stats(mesh)
+    if step_filename_format:
+        mesh.save(
+            step_filename_format.format(k),
+            show_coedges=False,
+            show_axes=False,
+            cell_quality_coloring=("viridis", 0.0, 1.0, False),
+        )
+
+    if callback:
+        callback(k, mesh)
+
+    # mesh.write("out0.vtk")
+    mesh.flip_until_delaunay()
+    # mesh.write("out1.vtk")
+
+    while True:
+        k += 1
+
+        new_points = get_new_points(mesh)
+
+        # Move boundary points to the domain boundary, if given. If not just move the
+        # points back to their original positions.
+        idx = mesh.is_boundary_point
+        if boundary_step is None:
+            # Reset boundary points to their original positions.
+            new_points[idx] = mesh.points[idx]
+        else:
+            # Move all boundary points back to the boundary.
+            new_points[idx] = boundary_step(new_points[idx].T).T
+
+        diff = omega * (new_points - mesh.points)
+
+        # Some methods are stable (CPT), others can break down if the mesh isn't very
+        # smooth. A break-down manifests, for example, in a step size that lets
+        # triangles become completely flat or even "overshoot". After that, anything can
+        # happen. To prevent this, restrict the maximum step size to half of the minimum
+        # the incircle radius of all adjacent cells. This makes sure that triangles
+        # cannot "flip".
+        # <https://stackoverflow.com/a/57261082/353337>
+        max_step = numpy.full(mesh.points.shape[0], numpy.inf)
+        numpy.minimum.at(
+            max_step,
+            mesh.cells["points"].reshape(-1),
+            numpy.repeat(mesh.cell_inradius, 3),
+        )
+        max_step *= 0.5
+        #
+        step_lengths = numpy.sqrt(numpy.einsum("ij,ij->i", diff, diff))
+        # alpha = numpy.min(max_step / step_lengths)
+        # alpha = numpy.min([alpha, 1.0])
+        # diff *= alpha
+        idx = step_lengths > max_step
+        diff[idx] *= max_step[idx, None] / step_lengths[idx, None]
+
+        new_points = mesh.points + diff
+
+        # project all points back to the surface, if any
+        if implicit_surface is not None:
+            fval = implicit_surface.f(new_points.T)
+            while numpy.any(numpy.abs(fval) > implicit_surface_tol):
+                grad = implicit_surface.grad(new_points.T)
+                grad_dot_grad = numpy.einsum("ij,ij->j", grad, grad)
+                # The step is chosen in the direction of the gradient with a step size
+                # such that, if the function was linear, the boundary (fval=0) would be
+                # hit in one step.
+                new_points -= (grad * (fval / grad_dot_grad)).T
+                # compute new value
+                fval = implicit_surface.f(new_points.T)
+
+        mesh.points = new_points
+        mesh.flip_until_delaunay()
+        # mesh.show(control_volume_centroid_color="C1")
+        # mesh.show()
+
+        # Abort the loop if the update was small
+        diff_norm_2 = numpy.einsum("ij,ij->i", diff, diff)
+        is_final = numpy.all(diff_norm_2 < tol ** 2) or k >= max_num_steps
+
+        if is_final or step_filename_format:
+            if is_final:
+                info = f"{k} steps"
+                if method_name is not None:
+                    if abs(omega - 1.0) > 1.0e-10:
+                        method_name += f", relaxation parameter {omega}"
+                    info += " of " + method_name
+
+                if verbose:
+                    print(f"\nFinal ({info}):")
+                    print_stats(mesh)
+            if step_filename_format:
+                mesh.save(
+                    step_filename_format.format(k),
+                    show_coedges=False,
+                    show_axes=False,
+                    cell_quality_coloring=("viridis", 0.0, 1.0, False),
+                )
+        if callback:
+            callback(k, mesh)
+
+        if is_final:
+            break
+
+    return k, numpy.max(numpy.sqrt(diff_norm_2))
--- optimesh/odt/nonlinear.py
+++ optimesh/odt/nonlinear.py
@@ -10,10 +10,8 @@ import numpy
 import quadpy
 from meshplex import MeshTri
 
-from .helpers import get_new_points_averaged, print_stats, runner
 
-
-def energy(mesh, uniform_density=False):
+def _energy(mesh, uniform_density=False):
     """The mesh energy is defined as
 
     E = int_Omega |u_l(x) - u(x)| rho(x) dx
@@ -55,83 +53,6 @@ def energy(mesh, uniform_density=False):
 
     assert out >= val
     return out - val
-
-
-def fixed_point_uniform(points, cells, *args, boundary_step=None, **kwargs):
-    """Idea:
-    Move interior mesh points into the weighted averages of the circumcenters
-    of their adjacent cells. (Except on boundary cells; use barycenters there.)
-    """
-
-    def get_new_points(mesh):
-        # Get circumcenters everywhere except at cells adjacent to the boundary;
-        # barycenters there.
-        cc = mesh.cell_circumcenters
-        bc = mesh.cell_barycenters
-        # Find all cells with a boundary edge
-        cc[mesh.is_boundary_cell] = bc[mesh.is_boundary_cell]
-        X = get_new_points_averaged(mesh, cc, mesh.cell_volumes)
-        if boundary_step is None:
-            # Reset boundary points to their original positions.
-            idx = mesh.is_boundary_point
-            X[idx] = mesh.points[idx]
-        else:
-            # Move all boundary points back to the boundary.
-            idx = mesh.is_boundary_point
-            X[idx] = boundary_step(X[idx].T).T
-        return X
-
-    mesh = MeshTri(points, cells)
-    runner(
-        get_new_points,
-        mesh,
-        *args,
-        **kwargs,
-        method_name="Optimal Delaunay Tesselation (ODT), uniform density, fixed-point variant",
-    )
-    return mesh.points, mesh.cells["points"]
-
-
-def fixed_point_density_preserving(points, cells, *args, boundary_step=None, **kwargs):
-    """Idea:
-    Move interior mesh points into the weighted averages of the circumcenters
-    of their adjacent cells.
-    """
-
-    def get_new_points(mesh):
-        # Get circumcenters everywhere except at cells adjacent to the boundary;
-        # barycenters there. The reason is that points near the boundary would be
-        # "sucked" out of the domain if the boundary cell is very flat, i.e., its
-        # circumcenter is very far outside of the domain.
-        # This heuristic also applies to cells _near_ the boundary though, and, if
-        # constructed maliciously, any mesh. Hence, this method can break down. A better
-        # approach is to use barycenters for all cells which are rather flat.
-        cc = mesh.cell_circumcenters.copy()
-        # Find all cells with a boundary edge
-        is_boundary_cell = (
-            numpy.sum(mesh.is_boundary_point[mesh.cells["points"]], axis=1) == 2
-        )
-        cc[is_boundary_cell] = mesh.cell_barycenters[is_boundary_cell]
-        X = get_new_points_averaged(mesh, cc)
-        if boundary_step is None:
-            # Reset boundary points to their original positions.
-            idx = mesh.is_boundary_point
-            X[idx] = mesh.points[idx]
-        else:
-            # Move all boundary points back to the boundary.
-            idx = mesh.is_boundary_point
-            X[idx] = boundary_step(X[idx].T).T
-        return X
-
-    mesh = MeshTri(points, cells)
-    runner(
-        get_new_points,
-        mesh,
-        *args,
-        **kwargs,
-        method_name="Optimal Delaunay Tesselation (ODT), density-preserving, fixed-point variant",
-    )
-    return mesh.points, mesh.cells["points"]
 
 
 def nonlinear_optimization_uniform(
@@ -176,7 +97,7 @@ def nonlinear_optimization_uniform(
 
     if verbose:
         print("Before:")
-        extra_cols = ["energy: {:.5e}".format(energy(mesh))]
+        extra_cols = ["energy: {:.5e}".format(_energy(mesh))]
         print_stats(mesh, extra_cols=extra_cols)
 
     def f(x):
