6dc8150bbe99da772ad2c7ba3c8bb340afb93d8b
Nico Schlömer <nico.schloemer@gmail.com>
2020-12-11 12:32:40 +0100

    simplify tests

--- test/test_cpt.py
+++ test/test_cpt.py
@@ -6,6 +6,7 @@ from meshplex import MeshTri
 import optimesh
 from optimesh.cpt.quasi_newton import _jac_uniform
 
+from .helpers import assert_norm_equality
 from .meshes import circle_random2, pacman, simple0, simple1, simple2, simple3
 
 
@@ -70,43 +71,33 @@ def test_simple1_jac():
 
 
 @pytest.mark.parametrize(
-    "mesh, ref1, ref2, refi",
+    "mesh, ref",
     [
-        (simple1, 0.96, 0.3262279745178587, 29.0 / 225.0),
-        (pacman, 12.35078985438217, 0.5420691555930099, 0.10101179397867549),
+        (simple1, [0.96, 0.3262279745178587, 29.0 / 225.0]),
+        (pacman, [12.35078985438217, 0.5420691555930099, 0.10101179397867549]),
     ],
 )
-def test_jac(mesh, ref1, ref2, refi):
+def test_jac(mesh, ref):
     X, cells = mesh()
-
     jac = _jac_uniform(X, cells)
-
-    nc = jac.flatten()
-    norm1 = numpy.linalg.norm(nc, ord=1)
-    norm2 = numpy.linalg.norm(nc, ord=2)
-    normi = numpy.linalg.norm(nc, ord=numpy.inf)
-
-    tol = 1.0e-12
-    assert abs(norm1 - ref1) < tol * ref1
-    assert abs(norm2 - ref2) < tol * ref2
-    assert abs(normi - refi) < tol * refi
+    assert_norm_equality(jac, ref, 1.0e-12)
 
 
 @pytest.mark.parametrize(
-    "method, mesh, ref1, ref2, refi",
+    "method, mesh, ref",
     [
-        ("cpt (fixed-point)", simple1, 5.0, 2.1213203435596424, 1.0),
-        ("cpt (fixed-point)", simple2, 7.390123456790124, 2.804687217072868, 1.7),
-        ("cpt (fixed-point)", simple3, 12.0, 3.9765648779799356, 2.0),
-        ("cpt (fixed-point)", pacman, 1901.5304112865315, 74.62452940437535, 5.0),
+        ("cpt (fixed-point)", simple1, [5.0, 2.1213203435596424, 1.0]),
+        ("cpt (fixed-point)", simple2, [7.390123456790124, 2.804687217072868, 1.7]),
+        ("cpt (fixed-point)", simple3, [12.0, 3.9765648779799356, 2.0]),
+        ("cpt (fixed-point)", pacman, [1901.5304112865315, 74.62452940437535, 5.0]),
         #
-        ("cpt (quasi-newton)", simple1, 5.0, 2.1213203435596424, 1.0),
-        ("cpt (quasi-newton)", simple2, 7.390123456790124, 2.804687217072868, 1.7),
-        ("cpt (quasi-newton)", simple3, 12.0, 3.976564877979913, 2.0),
-        ("cpt (quasi-newton)", pacman, 1900.910794007578, 74.58866209782154, 5.0),
+        ("cpt (quasi-newton)", simple1, [5.0, 2.1213203435596424, 1.0]),
+        ("cpt (quasi-newton)", simple2, [7.390123456790124, 2.804687217072868, 1.7]),
+        ("cpt (quasi-newton)", simple3, [12.0, 3.976564877979913, 2.0]),
+        ("cpt (quasi-newton)", pacman, [1900.910794007578, 74.58866209782154, 5.0]),
     ],
 )
-def test_methods(method, mesh, ref1, ref2, refi):
+def test_methods(method, mesh, ref):
     X_in, cells_in = mesh()
 
     # X_before = X_in.copy()
@@ -118,39 +109,20 @@ def test_methods(method, mesh, ref1, ref2, refi):
     # assert numpy.all(numpy.abs(X_in == X_before) < 1.0e-15)
 
     # Test if we're dealing with the mesh we expect.
-    nc = X.flatten()
-    norm1 = numpy.linalg.norm(nc, ord=1)
-    norm2 = numpy.linalg.norm(nc, ord=2)
-    normi = numpy.linalg.norm(nc, ord=numpy.inf)
-
-    tol = 1.0e-12
-    assert abs(norm1 - ref1) < tol * ref1
-    assert abs(norm2 - ref2) < tol * ref2
-    assert abs(normi - refi) < tol * refi
+    assert_norm_equality(X, ref, 1.0e-12)
 
 
 @pytest.mark.parametrize(
-    "mesh, ref1, ref2, refi",
+    "mesh, ref",
     [
-        (simple1, 5.0, 2.1213203435596424, 1.0),
-        (pacman, 1864.2406342781524, 73.19722600427883, 5.0),
+        (simple1, [5.0, 2.1213203435596424, 1.0]),
+        (pacman, [1864.2406342781524, 73.19722600427883, 5.0]),
     ],
 )
-def test_density_preserving(mesh, ref1, ref2, refi):
+def test_density_preserving(mesh, ref):
     X, cells = mesh()
-
     X, cells = optimesh.optimize_points_cells(X, cells, "cpt (linear solve)", 0.0, 10)
-
-    # Test if we're dealing with the mesh we expect.
-    nc = X.flatten()
-    norm1 = numpy.linalg.norm(nc, ord=1)
-    norm2 = numpy.linalg.norm(nc, ord=2)
-    normi = numpy.linalg.norm(nc, ord=numpy.inf)
-
-    tol = 1.0e-12
-    assert abs(norm1 - ref1) < tol * ref1
-    assert abs(norm2 - ref2) < tol * ref2
-    assert abs(normi - refi) < tol * refi
+    assert_norm_equality(X, ref, 1.0e-12)
 
 
 def test_circle():
